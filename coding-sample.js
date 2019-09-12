de4374d21993671e12620d31ce82b90bf7d7598d
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-09-12 14:26:40 +0800

    Initial commit

---
+++ coding-sample.js
@@ -0,0 +1,14 @@
+/* 
+  I have an inbox full of emails.The addresses are stored in a list called “allEmails”.
+  I also have a list of my friends’ email addresses called “friendList”.
+  I want to know how many of the emails in allEmails are from friends.
+  Write a function/method that takes in “friendList” and “allEmails” as parameters, 
+    then returns the number of emails in allEmails that are from a friend. 
+  Do not print the result, just return the result.  You can assume both lists are not null.  
+
+  Example:
+  friendList = “friend@catena-technologies.com”, “bff@uni.edu.sg”, “provost@uni.edu.sg”, “john@doe.com”
+  allEmails = “enemy@uni.edu.sg”, “bff@uni.edu.sg”, “evil@spam.com”, “bff@uni.edu.sg”, “friend@catena-technologies.com”
+
+  return: 3
+*/
